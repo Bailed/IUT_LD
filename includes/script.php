9ce74dd4b2c643635739eddc86d98214b7393875
Bailed <besson.maxime@gmail.com>
2015-02-20 17:27:57 +0100

    Structure complète

---
+++ includes/script.php
@@ -0,0 +1,11 @@
+<script src="js/libs/jquery/dist/jquery.min.js"></script>
+<script src="js/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+        $(document).ready(function () {
+            $('.dropdown-toggle').dropdown();
+        });
+</script>
+<script src="js/nlform.js"></script>
+<script>
+	var nlform = new NLForm( document.getElementById( 'nl-form' ) );
+</script>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include 'model/model.php';
+include 'includes/head.php';
+include 'includes/nav.php';
+?>
+<div class="main clearfix">
+	<form id="nl-form" class="nl-form">
+		Je souhaite ajouter 
+		<select>
+			<option value="1" selected>un étudiant</option>
+			<option value="2">un professeur</option>
+			<option value="3">une matière</option>
+		</select> à
+		<select>
+			<option value="1" selected>un groupe</option>
+			<option value="2">une promotion</option>
+			<option value="3">département informatique</option>
+		</select>
+		<button class="nl-submit text-center" type="submit">Envoyer ma demande</button>
+		<div class="nl-overlay"></div>
+	</form>
+</div>
+<?php
+include 'includes/footer.php';
+include 'includes/script.php';
+include 'includes/foot.php';
+?>
